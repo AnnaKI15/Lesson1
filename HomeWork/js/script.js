02cf350f4dc2812f9a8de11f15bf3d92a1bb41d9
Anna <hannakorbylo@gmail.com>
2024-12-16 23:26:36 +0200

    Push Lesson

--- HomeWork/js/script.js
+++ HomeWork/js/script.js
@@ -4,36 +4,47 @@
 // Присвойте значення "Іван" змінній name.
 // Скопіюйте значення зі змінної name в city.
 // Виведіть значення змінної city, використовуючи функцію console.log (яка повинна показати “Іван”).
+// let name;
+// let city;
+// name = "Іван";
+// city = name;
+// console.log(name);
+// console.log(city);
 
 //***2***
 //Який буде результат виконання скрипта?
 // let name = "Olga";
-// console.log(`привіт ${1}`); //
-// console.log(`привіт ${"name"}`); //
-// console.log(`привіт ${name}`); // ?
+// console.log(`привіт ${1}`); // привіт 1
+// console.log(`привіт ${"name"}`); // привіт name
+// console.log(`привіт ${name}`); // ?  привіт Olga
 
 //***3***
 //Видобути число зі змінних
 // let a = "5";
 // let b = "13cvb";
 // let c = "12.9sxdcfgv";
-// вивести в консоль тип
-
+// // вивести в консоль тип
+// console.log(typeof Number.parseInt(a));
+// console.log(typeof Number.parseInt(b));
+// console.log(typeof Number.parseFloat(c));
 //***4***
 //Зробіть, щоб 0.1 + 0.2 = 0.3
-
+// console.log((0.1  * 10 + 0.2 * 10)/10);
 //***5**
 //Поверніть найбільше число с набору 20, 10, 50, 40
-
+// console.log(Math.max(20, 10, 50, 40));
 //***6**
 //Поверніть випадкове число в діапазоні від 2 до 4
-
+// console.log(Math.random()*(2-4)+2);
 //***7**
 //дізнатись довжину message
 // const message = "Welcome to Bahamas!";
+// console.log(message.length);
 
 //***8**
 //вивести в консоль message  великими літерами
+// console.log(message.toUpperCase());
+
 
 //***9**
 // створити пустий об*єкт
@@ -42,6 +53,20 @@
 // видалити місто
 // додати буль з ключем: like flowers
 // вивести результат в консоль
+// let obj = {};
+// obj.name = "Ivan";
+// obj.age = "21";
+// obj.city = "Kyiv";
+//  console.log(obj);
+
+// delete obj.city;
+
+// obj["like flowers"] = true;
+// console.log(obj);
 
 //***10**
 // За допомогою циклу  “for…in” вивести в консоль ключі і значення об*єкта
+// for(let key in obj){
+//     console.log(key);
+//     console.log(obj[key]);
+// }
